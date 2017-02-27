224ddd5c9760014a2dde0d081942af9837105052
Erica Wright and Zach Swanson <erica@>
2017-02-27 10:20:45 -0800

    create initial files and folders

---
+++ src/City.php
@@ -0,0 +1,32 @@
+<?php
+    class City
+    {
+        private $id;
+        private $name;
+
+        function __construct($name, $id = null)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setName($new_name)
+        {
+            $this->name = (string) $new_name;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+    }
+
+
+
+
+?>
